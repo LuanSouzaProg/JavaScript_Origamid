6c6d587969a1e8b3427aec2ba4aaae101e8faed9
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-08-31 11:11:50 -0300

    JavaScript-Origamid

--- Array/script_Atividade.js
+++ Array/script_Atividade.js
@@ -5,6 +5,13 @@
 
 const comidasAtividades = ['Pizza','Frango','Carne','Macarrão',];
 
+const primeiroValor = comidasAtividades.shift();
+const ultimoValor = comidasAtividades.pop();
+comidasAtividades.push('Arroz');
+comidasAtividades.unshift('Peixe', 'Batata');
+
+console.log(comidasAtividades);
+
 //Arrume os estudantes em ordem alfabética
 //inverta a ordem dos estudantes
 //verifique se joana faz parte dos estudantes
@@ -12,8 +19,27 @@ const comidasAtividades = ['Pizza','Frango','Carne','Macarrão',];
 
 const estudantesAtividades = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia',];
 
+estudantesAtividades.sort();
+estudantesAtividades.reverse();
+
+console.log(estudantesAtividades);
+console.log(estudantesAtividades.includes('Joana'));
+console.log(estudantesAtividades.includes('Juliana'));
+
 //substitua section por ul e div com li utilizando split e join
 
 let htmlAtividades = '<section><div>Sobre</div><div>Produtos</div><div>Contato</div></section>';
 
+htmlAtividades = htmlAtividades.split('section').join('ul').split('div').join('li');
+
+console.log(htmlAtividades);
+
 //remova o último carro, mas antes de remover salve a array original em outra variável
+
+const carrosAtividades = ['Ford', 'Fiat', 'VW', 'Honda'];
+const carrosCopia = carrosAtividades.slice();
+
+carrosAtividades.pop();
+
+console.log(carrosAtividades);
+console.log(carrosCopia);
