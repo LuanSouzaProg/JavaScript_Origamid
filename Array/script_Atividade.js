64760c470e3a83b89fb5fbb1ba5ecef642e01a3f
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-08-31 10:55:35 -0300

    JavaScript-Origamid

---
+++ Array/script_Atividade.js
@@ -0,0 +1,19 @@
+//Remova o primeiro valor de comidas e coloque em uma variável
+//Remova o último valor de comidas e coloque em uma variável
+//adicione 'Arroz' ao final da array
+//adicione 'Peixe' e 'Batata' ao inicio da array
+
+const comidasAtividades = ['Pizza','Frango','Carne','Macarrão',];
+
+//Arrume os estudantes em ordem alfabética
+//inverta a ordem dos estudantes
+//verifique se joana faz parte dos estudantes
+//verifique se juliana faz parte dos estudantes
+
+const estudantesAtividades = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia',];
+
+//substitua section por ul e div com li utilizando split e join
+
+let htmlAtividades = '<section><div>Sobre</div><div>Produtos</div><div>Contato</div></section>';
+
+//remova o último carro, mas antes de remover salve a array original em outra variável
--- Array/script_aula.js
+++ Array/script_aula.js
@@ -51,17 +51,50 @@ const carros2 = ['Ford', 'Fiat', 'VW'];
 carros2.unshift('Honda', 'Kia',); // add os itens dígitados no inicio da array
 console.log(carros2);
 
-carros2.push('Ferrari'); // add os itens dígitados no fila na array
+//carros2.push('Ferrari'); // add os itens dígitados no fila na array
+//console.log(carros2);
+
+//console.log(carros2.pop()); //tira o último item da array
+//console.log(carros2);
+
+//console.log(carros2.shift()); //tira o primeiro item da array
+//console.log(carros2);
+
+//console.log(carros2.reverse()); //inverte a ordem da array
+
+
+console.log(carros2);
+console.log(carros2.splice(2,0, 'Fusca'));
+
 console.log(carros2);
 
-console.log(carros2.pop()); //tira o último item da array
-console.log(carros2);
+console.log(['Item1', 'Item2', 'Item3', 'Item4', 'Item5'].copyWithin(2, 0, 3));
 
-console.log(carros2.shift()); //tira o primeiro item da array
-console.log(carros2);
+console.log(['Item1', 'Item2', 'Item3', 'Item4', 'Item5'].fill('Banana', 0, 2));
 
-console.log(carros2.reverse()); //inverte a ordem da array
+const transporte1 = ['Barco', 'Avião'];
+const transporte2 = ['Carro', 'Moto'];
 
+const todosTransportes = transporte1.concat(transporte2);
+
+console.log(todosTransportes);
+
+const linguagens = ['html', 'css', 'js', 'php', 'python', 'js', ];
+
+console.log(linguagens.includes('css'));//procura para ver se tem na array a palavra dígitada, mostrando true or false
+console.log(linguagens.includes('ruby'));
+console.log(linguagens.indexOf('python'));//mostra em qual lugar da array o item dígitado está
+console.log(linguagens.indexOf('js'));
+console.log(linguagens.lastIndexOf('js'));//mostra o último lugar que o item foi dígitado
+
+console.log(linguagens.slice(2,4)); //retorna o item entre as posições dígitadas
+
+
+let htmlString = '<h2>Título Principal</h2>';
+htmlString = htmlString.split('h2');
+htmlString = htmlString.join('h1');
+
+console.log(htmlString);
 
 
 
