8cf5714606beee87f5cde96d911fe848ec9a499b
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-09-11 07:27:45 -0300

    JavaScript-Origamid

--- Dataset/animacoes.js
+++ Dataset/animacoes.js
@@ -1,6 +1,6 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if(tabMenu.length && tabContent.length) {
     tabContent[0].classList.add('ativo');
@@ -9,7 +9,10 @@ function initTabNav() {
       tabContent.forEach((section) => {
         section.classList.remove('ativo');
       });
-      tabContent[index].classList.add('ativo');
+
+      const direcao =  tabContent[index].dataset.anime;
+
+      tabContent[index].classList.add('ativo', direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -22,7 +25,7 @@ function initTabNav() {
 initTabNav();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const accordionList = document.querySelectorAll('[data-anime="accordin"] dt');
   const activeClass = 'ativo';
   
   if(accordionList.length) {
@@ -42,7 +45,7 @@ function initAccordion() {
 initAccordion();
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -68,7 +71,7 @@ function initScrollSuave() {
 initScrollSuave();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
   if(sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
---
+++ Dataset/atividades.js
@@ -0,0 +1,16 @@
+//adicione um atributo data-anime = "show-down" e data-anime = "show-right" a todas as section's
+//com descrição dos animais 
+
+
+
+//utilizando estes atributos, adicione a classe show-down e show-right a sua respectiva classe assim
+//que a mesma aparecer na tela (animacao tab)
+
+
+
+//No CSS faça com que o show-down anime de cima para baixo e show-righ continue com a mesma animação da esquerda
+//para a direita
+
+
+
+//substitua todas as classes js- por data atributes
