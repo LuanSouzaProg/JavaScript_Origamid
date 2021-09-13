ebad698ad0c0d2c2cda11a512905bbc3aa7952b8
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-09-13 06:45:30 -0300

    JavaScript-Origamid

---
+++ modules_introducao/js/modules/teste.js
@@ -0,0 +1,11 @@
+export function teste1(){
+    console.log('Isso é um teste');
+}
+
+export function teste2(){
+    console.log('Isso é um teste 02');
+}
+
+var nome = 'Luan';
+
+export const senha = 202589864664;
---
+++ modules_introducao/js/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from "./modules/scroll_suave.js";
+import initAnimacaoScroll from "./modules/scroll_animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import * as teste from "./modules/teste.js";
+
+teste.teste1();
+teste.teste2();
+console.log(teste.senha);
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
