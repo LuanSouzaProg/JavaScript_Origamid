ceb225e9bbcad140d3ee4d24465653e110f36961
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-09-13 08:25:58 -0300

    JavaScript-Origamid

---
+++ modal/js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from "./modules/scroll_suave.js";
+import initAnimacaoScroll from "./modules/scroll_animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
