be3faf01f54d8296747dd1708488f29983653eae
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2021-10-04 18:30:20 -0300

    JavaScript-Origamid

--- Fetch/script_aula.js
+++ Fetch/script_aula.js
@@ -37,3 +37,41 @@ paginaSobre.then((resolucao)=>{
     const titulo = div.querySelector('h1');
     document.querySelector('h1').innerText = titulo.innerText;
 });
+
+const img = fetch("./teste.jpg");
+
+img.then(resolucao => resolucao.blob())
+.then(body => {
+    const blobUrl = URL.createObjectURL(body);
+    const imgDom = document.querySelector('img');
+    imgDom.src = blobUrl;
+});
+
+const TestClone = fetch("https://viacep.com.br/ws/88980-000/json/");
+
+TestClone.then(resolucao => {
+    const resolucao2 = resolucao.clone();
+    resolucao.text().then((text)=>{
+        //console.log(text)
+    });
+    resolucao2.json().then((json)=>{
+        //console.log(json);
+    });
+}).then(body => {
+    console.log(body);
+});
+
+const testStatus = fetch("https://viacep.com.br/ws/88980-000/json/");
+
+testStatus.then(response => {
+    console.log(response.status);
+    console.log(response.type);
+    console.log(response.url);
+
+    if(response.status === 404){
+        console.log('Página não existe');
+    }else if(response.status === 200){
+        console.log('Página existe')
+    }
+});
+
